9ab36613b563a7c5b856b6a6416a3175e61f5768
kashew-developers <kashewdevelopers@gmail.com>
2020-11-24 10:29:44 +0530

    translate About from java to kotlin

---
+++ app/src/main/java/in/kashewdevelopers/mirrortext/About.kt
@@ -0,0 +1,20 @@
+package `in`.kashewdevelopers.mirrortext
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class About : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+    fun gitHubLink(view: View) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.github_url))))
+    }
+
+}
